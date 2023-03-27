94cddadd33f4dfde10a9695a14e5946553708e96
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-27 21:13:04 +0100

    Create signup component and add scene render logic to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { setCurrentScene } from "./redux-store/scene/sceneSlice";
 import ReduxComp from "./components/redux/ReduxComp";
 import StartMenu from "./components/start-menu/StartMenu.component";
 import Login from "./components/login/Login.component";
+import Signup from "./components/signup/Signup.component";
 
 function App() {
   const dispatch = useDispatch();
@@ -11,6 +12,7 @@ function App() {
     <div className="flex flex-col justify-center items-center overflow-hidden h-screen">
       {currentScene === "START_MENU" && <StartMenu />}
       {currentScene === "LOGIN" && <Login />}
+      {currentScene === "SIGNUP" && <Signup />}
     </div>
   );
 }
