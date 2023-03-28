34cf9b930d289d8e846a6add76c2682edf44ba59
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-28 12:07:50 +0100

    Setup useEffect to render bedroom if token is present

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,21 @@ import StartMenu from "./components/start-menu/StartMenu.component";
 import Login from "./components/login/Login.component";
 import Signup from "./components/signup/Signup.component";
 import Bedroom from "./components/bedroom/Bedroom.component";
+import { useEffect } from "react";
 
 function App() {
   const dispatch = useDispatch();
   const { currentScene } = useSelector((state) => state.scene);
+
+  useEffect(() => {
+    if (
+      window.localStorage.getItem("token") &&
+      window.localStorage.getItem("token") !== "undefined"
+    ) {
+      dispatch(setCurrentScene("BEDROOM"));
+    }
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center overflow-hidden h-screen">
       {currentScene === "START_MENU" && <StartMenu />}
