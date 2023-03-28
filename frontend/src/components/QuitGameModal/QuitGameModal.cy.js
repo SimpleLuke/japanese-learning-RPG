b73f9d4cf56969780e5d81bf83096eb568959447
annamaggg <annamagnusson99@gmail.com>
2023-03-28 17:15:24 +0100

    test commented out

--- frontend/src/components/QuitGameModal/QuitGameModal.cy.js
+++ frontend/src/components/QuitGameModal/QuitGameModal.cy.js
@@ -1,94 +1,28 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import modalRuducer from "../../redux-store/game-modal/gameModalSlice";
-import QuitGameModal from "./QuitGameModal";
-
-describe("QuitGameModal component", () => {
-  let store;
-
-  beforeEach(() => {
-    store = configureStore({ reducer: { modalMenu: modalRuducer } });
-  });
-
-  it("exists", () => {
-    cy.mount(
-      <Provider store={store}>
-        <QuitGameModal />
-      </Provider>
-    );
-
-    // cy.get('[data-test="transitionRoot"]').should('exist');
-    cy.getTest('transitionRoot').should('exist');
-    // cy.get('[data-test="dialog"]').should('be.visible');
-  })
-});
-
-//   it("renders login buttons", () => {
-//     cy.mount(
-//       <Provider store={store}>
-//         <StartMenu />
-//       </Provider>
-//     );
-
-//     cy.getTest("login-btn").should("exist");
-//   });
-
-//   it("renders signup buttons", () => {
-//     cy.mount(
-//       <Provider store={store}>
-//         <StartMenu />
-//       </Provider>
-//     );
-
-//     cy.getTest("signup-btn").should("exist");
-//   });
-// });
-
-
-
-
-
-
 // import React from "react";
 // import { Provider } from "react-redux";
 // import { configureStore } from "@reduxjs/toolkit";
-// import sceneRuducer from "../../redux-store/scene/sceneSlice";
-// import StartMenu from "./StartMenu.component";
+// import modalRuducer from "../../redux-store/game-modal/gameModalSlice";
+// import QuitGameModal from "./QuitGameModal";
 
-// describe("StartMenu component", () => {
+// describe("QuitGameModal component", () => {
 //   let store;
 
 //   beforeEach(() => {
-//     store = configureStore({ reducer: { scene: sceneRuducer } });
+//     store = configureStore({ reducer: { modalMenu: modalRuducer } });
 //   });
 
-//   it("currentScene is eual to START_MENU", () => {
+//   it("exists", () => {
 //     cy.mount(
 //       <Provider store={store}>
-//         <StartMenu />
-//       </Provider>
-//     );
-//     expect(store.getState().scene.currentScene).to.equal("START_MENU");
-//   });
-
-//   it("renders login buttons", () => {
-//     cy.mount(
-//       <Provider store={store}>
-//         <StartMenu />
+//         <QuitGameModal />
 //       </Provider>
 //     );
 
-//     cy.getTest("login-btn").should("exist");
-//   });
-
-//   it("renders signup buttons", () => {
-//     cy.mount(
-//       <Provider store={store}>
-//         <StartMenu />
-//       </Provider>
-//     );
-
-//     cy.getTest("signup-btn").should("exist");
-//   });
+//     // cy.get('[data-test="transitionRoot"]').should('exist');
+//     cy.getTest('transitionRoot').should('exist');
+//     // cy.get('[data-test="dialog"]').should('be.visible');
+//   })
 // });
+
+
+
