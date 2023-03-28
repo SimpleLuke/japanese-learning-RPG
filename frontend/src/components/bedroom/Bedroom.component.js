1602c2ec1a84ac31a8713b813abe221b14eb72ff
Luke Lai <lukelai.dev@gmail.com>
2023-03-28 21:49:30 +0100

    Add Bedroom component test

--- frontend/src/components/bedroom/Bedroom.component.js
+++ frontend/src/components/bedroom/Bedroom.component.js
@@ -9,20 +9,33 @@ const Bedroom = () => {
   return (
     <div className="bg-bedroom bg-cover bg-center h-screen w-screen grid grid-cols-4 grid-rows-4 gap-4 p-4 ">
       <div className="w-80 h-40 overflow-hidden rounded-lg bg-white bg-opacity-80 px-4 py-5 shadow sm:p-6">
-        <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+        <dd
+          data-test="email"
+          className="mt-1 text-3xl font-semibold tracking-tight text-gray-900"
+        >
           {email}
         </dd>
-        <dt className="truncate text-sm font-medium text-gray-500">
+        <dt
+          data-test="level"
+          className="truncate text-sm font-medium text-gray-500"
+        >
           Level: {level}
         </dt>
-        <dt className="truncate text-sm font-medium text-gray-500">
+        <dt
+          data-test="exp"
+          className="truncate text-sm font-medium text-gray-500"
+        >
           Exp: {xp}
         </dt>
-        <dt className="truncate text-sm font-medium text-gray-500">
+        <dt
+          data-test="words"
+          className="truncate text-sm font-medium text-gray-500"
+        >
           Words: {wordsKnown}
         </dt>
       </div>
       <div
+        data-test="study-desk"
         onClick={() => dispatch(setCurrentScene("GAME_START"))}
         className="w-40 h-40 col-start-4 row-start-2 cursor-pointer overflow-hidden rounded-lg bg-white bg-opacity-80 px-4 py-5 shadow sm:p-6"
       >
