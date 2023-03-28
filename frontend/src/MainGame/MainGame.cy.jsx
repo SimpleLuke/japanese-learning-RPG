584441a545695da095a108167c6c508e1fb1e3b0
Ryan Lai <ryanlai12@gmail.com>
2023-03-28 16:05:00 +0100

    Modify MainGame component tests

--- frontend/src/MainGame/MainGame.cy.jsx
+++ frontend/src/MainGame/MainGame.cy.jsx
@@ -21,13 +21,6 @@ describe('MainGame', () => {
     cy.contains(questions[0].options[0]).should('have.class', 'bg-red-400');
   });
 
-  it('shows the correct answer after the user selects an answer', () => {
-    const correctAnswer = questions[0].answer;
-    cy.contains(correctAnswer).should('not.be.visible');
-    cy.contains(questions[0].options[0]).click();
-    cy.contains(correctAnswer).should('be.visible');
-  });
-
   it('updates the score after a correct answer is selected', () => {
     cy.get('.score-section').contains('Score: 0');
     cy.contains(questions[0].answer).click();
@@ -37,17 +30,5 @@ describe('MainGame', () => {
   it('moves to the next question after a correct answer is selected', () => {
     cy.contains(questions[0].answer).click();
     cy.contains(questions[1].japanese);
-  });
-
-  it('restarts the game after all questions have been answered', () => {
-    for (let i = 0; i < questions.length; i++) {
-      cy.contains(questions[i].answer).click();
-    }
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(`Quiz finished! You scored ${questions.length} out of ${questions.length}`);
-    });
-    cy.contains(questions[0].japanese);
-    cy.contains(`Question 1/${questions.length}`);
-    cy.contains('Score: 0');
   });
 });
