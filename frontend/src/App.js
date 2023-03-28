5bd5e257d44d0c272545f5d6c24333897c539a24
James Mcleish <james.mcleish@icloud.com>
2023-03-28 18:09:09 +0100

    Add start and end game.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import ReduxComp from "./components/redux/ReduxComp";
+import StartGame from "./StartGame/StartGame";
+import EndGame from "./EndGame/EndGame";
 
 function App() {
   return (
     <div className="flex w-screen h-screen bg-blue-400">
-      <ReduxComp />
+      <EndGame/>
     </div>
   );
 }
