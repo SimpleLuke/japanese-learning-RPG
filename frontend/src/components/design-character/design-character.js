dbb85c718f1783925e68c9edaf9f78ef0ccb70ca
annamaggg <annamagnusson99@gmail.com>
2023-03-29 17:24:46 +0100

    character rendering to the scene

--- frontend/src/components/design-character/design-character.js
+++ frontend/src/components/design-character/design-character.js
@@ -1,12 +1,12 @@
 // import { setCurrentScene } from "../../redux-store/scene/sceneSlice";
-
-// import character1 from '../../../public/img/character-sprites/char1';
+import './design-character.css';
+import character1 from './character-sprites/char1.png';
 
 const CharacterDesign = () => {
   return (
-    // <div className='characterOneSprite'></div>
     <div>
       <h1>Character Design page</h1>
+      <div className='characterOneSprite'></div>
     </div>
   )
 }
