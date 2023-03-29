d32d7035e5354c0fe13c58d194ee22edec2907d1
annamaggg <annamagnusson99@gmail.com>
2023-03-29 20:04:20 +0100

    character with scaling complete

--- frontend/src/components/design-character/design-character.js
+++ frontend/src/components/design-character/design-character.js
@@ -6,11 +6,15 @@ const CharacterDesign = () => {
   return (
     <div>
       <h1>Character Design page</h1>
-      <div className='characterOneSprite'>
-        <div className='characterOneTshirt'>
-          <div className='characterOnePants'></div>
+        <div className='characterOneSprite'>
+          <div className='characterOneTshirt'>
+            <div className='characterOnePants'>
+              <div className='characterOneHair'>
+
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
     </div>
   )
 }
