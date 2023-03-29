c53b67861976b3f5d9bbf4b56540c42d7a3eef22
annamaggg <annamagnusson99@gmail.com>
2023-03-29 17:11:35 +0100

    character design component added and appears after signing up

---
+++ frontend/src/components/design-character/design-character.js
@@ -0,0 +1,14 @@
+// import { setCurrentScene } from "../../redux-store/scene/sceneSlice";
+
+// import character1 from '../../../public/img/character-sprites/char1';
+
+const CharacterDesign = () => {
+  return (
+    // <div className='characterOneSprite'></div>
+    <div>
+      <h1>Character Design page</h1>
+    </div>
+  )
+}
+
+export default CharacterDesign;
--- frontend/src/components/signup/Signup.component.js
+++ frontend/src/components/signup/Signup.component.js
@@ -107,6 +107,7 @@ const Signup = () => {
               <button
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => dispatch(setCurrentScene("CHARACTER"))}
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <LockClosedIcon
