6f30da8b37f0443998e3e263c37e741758ff2915
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-30 14:13:52 +0100

    Update userSlice with new methods for updating attributes, add XP to userSlice on EndGame screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,11 +13,9 @@ import LoadingWalk from "./components/loading/Loading.component.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<LoadingWalk />} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={<LoadingWalk />} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
