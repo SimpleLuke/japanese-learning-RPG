cb90287046f542ecb676a5cef5fc6017a48a98a9
annamaggg <annamagnusson99@gmail.com>
2023-03-30 16:45:29 +0100

    shoes added to characters

--- frontend/src/components/design-character/character.component.js
+++ frontend/src/components/design-character/character.component.js
@@ -1,12 +1,14 @@
 const CharacterComponent = ({data}) => {
-  const { body, hair, top, bottoms } = data;
+  const { body, hair, top, bottoms, shoes } = data;
 
   return (
     <>
     <div className={`${body}`}>
       <div className={`${hair}`}>
         <div className={`${top}`}>
-          <div className={`${bottoms}`}></div>
+          <div className={`${bottoms}`}>
+            <div className={`${shoes}`}></div>
+          </div>
         </div>
       </div>
     </div>    
