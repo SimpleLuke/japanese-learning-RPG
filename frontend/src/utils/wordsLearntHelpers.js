7d340507f750688ab69f2b5283c51a1a4782bc66
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-30 17:14:16 +0100

    Fix bug where newly learnt vocab doesn't render on the endscreen

--- frontend/src/utils/wordsLearntHelpers.js
+++ frontend/src/utils/wordsLearntHelpers.js
@@ -1,10 +1,7 @@
 const newWords = (wordsLearnt, wordsStudied) => {
-  console.log("newWords, wordsLearnt = :", wordsStudied);
   if (!wordsStudied || wordsStudied.length === 0) {
-    console.log("if triggered");
     return [];
   } else {
-    console.log("else triggered");
     return wordsStudied.filter((word) => !wordsLearnt.includes(word));
   }
 };
