098517ea188a1ae02406030e57c468cd5bd7b8b3
Luke Lai <lukelai.dev@gmail.com>
2023-03-30 16:26:13 +0100

    Add function to clear redux persist data when logout

--- frontend/src/redux-store/store.js
+++ frontend/src/redux-store/store.js
@@ -1,9 +1,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "reduxjs-toolkit-persist";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "reduxjs-toolkit-persist";
 import storage from "reduxjs-toolkit-persist/lib/storage";
 import autoMergeLevel1 from "reduxjs-toolkit-persist/lib/stateReconciler/autoMergeLevel1";
 
-import cartReducer from "./cart/cartSlice";
 import gameModalReducer from "./game-modal/gameModalSlice";
 import sceneReducer from "./scene/sceneSlice";
 import gameReducer from "./game/gameSlice";
@@ -16,7 +24,6 @@ const persistConfig = {
 };
 
 const reducers = combineReducers({
-  cart: cartReducer,
   modalMenu: gameModalReducer,
   scene: sceneReducer,
   game: gameReducer,
@@ -29,7 +36,9 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
 
