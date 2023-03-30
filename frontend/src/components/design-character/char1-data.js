55444263d2520ef2847765ce50bfdf1fb3aa37ef
annamaggg <annamagnusson99@gmail.com>
2023-03-30 13:42:39 +0100

    character component added and class names for css added

---
+++ frontend/src/components/design-character/char1-data.js
@@ -0,0 +1,33 @@
+const character1Data = {
+  sprite: {
+    width: '32px',
+    height: '32px',
+    position: 'absolute',
+    transform: 'scale(6)',
+    imageRendering: 'pixelated',
+  },  
+  spriteImage: {
+    image: '../../design-character/character-sprites/char1.png'
+  },
+  tshirt: {
+    width: '32px',
+    height: '32px',
+    image: '../../design-character/character-sprites/basic-clothes.png',
+    position: 'absolute',
+  },  
+  pants: {
+    width: '32px',
+    height: '32px',
+    image: '../../design-character/character-sprites/pants.png',
+    backgroundPosition:  '-512px 0px',
+    position: 'absolute',
+  },
+  hair: {
+    width: '32px',
+    height: '32px',
+    image: '../../design-character/character-sprites/gentleman-hair.png',
+    position: 'absolute',
+  }
+};
+
+export default character1Data;
