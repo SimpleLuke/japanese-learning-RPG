f5b72cbed05ef8eaf7ebd8c98e375e91125ae683
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-30 13:32:50 +0100

    create newWords helper function to work out which words are new and which have been studied previously

--- frontend/src/utils/wordsLearntHelpers.js
+++ frontend/src/utils/wordsLearntHelpers.js
@@ -1,1 +1,12 @@
+//temporary words learnt (can be replaced with real wordslearnt once implemented)
+let wordsLearnt = ["ごめんなさい", "ありがとう", "おはよう", "すみません"];
 
+const newWords = (wordsStudied) => {
+  return wordsStudied.filter((word) => {
+    if (!wordsLearnt.includes(word)) {
+      return word + ", ";
+    }
+  });
+};
+
+export { newWords };
