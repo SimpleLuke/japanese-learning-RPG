fd68022bc7342fe437af02ee476b024a1be77b49
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-31 11:32:31 +0100

    Create conditonal rendering whether new words are learnt or not

--- frontend/src/components/EndGame/EndGame.cy.jsx
+++ frontend/src/components/EndGame/EndGame.cy.jsx
@@ -1,5 +1,43 @@
-describe("Endgame", () => {
-  it("true", () => {
-    expect(true).to.equal(true);
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import sceneReducer from "../../redux-store/scene/sceneSlice";
+import userReducer from "../../redux-store/user/userSlice";
+
+import EndGame from "./EndGame";
+
+describe("EndGame component", () => {
+  let store;
+  beforeEach(() => {
+    store = configureStore({
+      reducer: { scene: sceneReducer },
+      preloadedState: {
+        scene: {
+          currentScene: "END_GAME",
+        },
+      },
+    });
   });
+
+  // it("currentScene is equal to LOGIN", () => {
+  //   cy.mount(
+  //     <Provider store={store}>
+  //       <EndGame />
+  //     </Provider>
+  //   );
+  //   expect(store.getState().scene.currentScene).to.equal("END_GAME");
+  // });
+
+  // it("renders endGame screen", () => {
+  //   cy.mount(
+  //     <Provider store={store}>
+  //       <EndGame />
+  //     </Provider>
+  //   );
+  //   cy.getTest("score").should("exist");
+  //   cy.getTest("coins").should("exist");
+  //   cy.getTest("words-studied").should("exist");
+  //   cy.getTest("XP").should("exist");
+  //   cy.getTest("endGameInstructions").should("exist");
+  // });
 });
