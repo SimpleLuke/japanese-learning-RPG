e933dc9b68b6d433510543cc327400f692ecc705
Luke Lai <lukelai.dev@gmail.com>
2023-04-02 17:36:30 +0100

    Modify icon positon and add shop component

--- frontend/src/components/bedroom/Bedroom.component.js
+++ frontend/src/components/bedroom/Bedroom.component.js
@@ -28,37 +28,44 @@ const Bedroom = () => {
       <div className="col-start-8 row-start-1 row-end-1">
         <BurgerMenu />
       </div>
-      <div className="col-start-1 row-start-1 w-80 h-40 overflow-hidden rounded-lg bg-white bg-opacity-80 px-4 py-5 shadow sm:p-6">
-        <dd
-          data-test="email"
-          className="mt-1 text-3xl font-semibold tracking-tight text-gray-900"
-        >
-          {email}
-        </dd>
-        <dt
-          data-test="level"
-          className="truncate text-sm font-medium text-gray-500"
-        >
-          Level: {level}
-        </dt>
-        <dt
-          data-test="exp"
-          className="truncate text-sm font-medium text-gray-500"
-        >
-          Exp: {xp}
-        </dt>
-        <dt
-          data-test="words"
-          className="truncate text-sm font-medium text-gray-500"
-        >
-          Words: {wordsKnown}
-        </dt>
-        <dt
-          data-test="coins"
-          className="truncate text-sm font-medium text-gray-500"
-        >
-          Coins: {coins}
-        </dt>
+      <div className="col-start-1 row-start-1 flex w-96 h-40 overflow-hidden rounded-lg bg-white bg-opacity-80 px-4 py-5 shadow sm:p-6">
+        <div>
+          <dd
+            data-test="email"
+            className="mt-1 text-3xl font-semibold tracking-tight text-gray-900"
+          >
+            {email}
+          </dd>
+          <dt
+            data-test="level"
+            className="truncate text-sm font-medium text-gray-500"
+          >
+            Level: {level}
+          </dt>
+          <dt
+            data-test="exp"
+            className="truncate text-sm font-medium text-gray-500"
+          >
+            Exp: {xp}
+          </dt>
+          <dt
+            data-test="words"
+            className="truncate text-sm font-medium text-gray-500"
+          >
+            Words: {wordsKnown}
+          </dt>
+          <dt
+            data-test="coins"
+            className="truncate text-sm font-medium text-gray-500"
+          >
+            Coins: {coins}
+          </dt>
+        </div>
+        <div className=" flex justify-center items-baseline w-40 ">
+          {character.currentOutfit && (
+            <CharacterComponent data={character.currentOutfit} />
+          )}
+        </div>
       </div>
       <div
         data-test="study-desk"
@@ -75,10 +82,12 @@ const Bedroom = () => {
       >
         Bookshelf
       </div>
-      <div className="ml-50 mt-100">
-        {character.currentOutfit && (
-          <CharacterComponent data={character.currentOutfit} />
-        )}
+      <div
+        data-test="shop"
+        onClick={() => dispatch(setCurrentScene("SHOP"))}
+        className="w-40 h-40 col-start-4 row-start-3 cursor-pointer overflow-hidden rounded-lg bg-white bg-opacity-80 px-4 py-5 shadow sm:p-6"
+      >
+        Shop
       </div>
     </div>
   );
