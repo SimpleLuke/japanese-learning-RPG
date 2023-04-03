d8c65ff8ab07d36a953c826663cb1721b58c4199
James Mcleish <james.mcleish@icloud.com>
2023-04-03 12:27:22 +0200

    verified stats posting and words learnt

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,34 +1,10 @@
 const express = require("express");
-
-
 const usersRouter = require("./users/users.router");
 const tokensRouter = require("./tokens/tokens.router");
 const gameRouter = require("./game/game.router");
-
 const api = express.Router();
+require('./../util/tokenChecker.js')
 
-const tokenChecker = (req, res, next) => {
-  const authHeader = req.get("Authorization");
-
-  if (authHeader) {
-    token = authHeader.slice(7);
-  }
-
-  if (token) {
-    JWT.verify(token, 'SECRET', (err, token) => {
-      if (err) {
-        console.log(err);
-        res.status(401).json({ message: "auth error" });
-      } else {
-        req.user_id = token.user_id;
-        res.json({message: "Verification ok."})
-        next();
-      }
-    });
-  } else {
-    res.status(401).json({ message: "No token provided" });
-  }
-};
 
 
 
--- frontend/src/components/EndGame/EndGame.component.jsx
+++ frontend/src/components/EndGame/EndGame.component.jsx
@@ -42,7 +42,7 @@ const EndGame = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: token,
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({
         email: email,
