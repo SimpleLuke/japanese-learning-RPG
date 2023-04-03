47f47af61599dc4aa8afb4003be0940fefe1ac41
James Mcleish <james.mcleish@icloud.com>
2023-04-03 12:05:56 +0200

    switching branches

--- backend/src/util/tokenChecker.mjs
+++ backend/src/util/tokenChecker.mjs
@@ -1,11 +1,3 @@
-const express = require("express");
-
-
-const usersRouter = require("./users/users.router");
-const tokensRouter = require("./tokens/tokens.router");
-const gameRouter = require("./game/game.router");
-
-const api = express.Router();
 
 const tokenChecker = (req, res, next) => {
   const authHeader = req.get("Authorization");
@@ -30,11 +22,4 @@ const tokenChecker = (req, res, next) => {
   }
 };
 
-
-
-
-api.use("/users", usersRouter);
-api.use("/tokens", tokensRouter);
-api.use("/game", tokenChecker, gameRouter);
-
-module.exports = api;
+export {tokenChecker};
