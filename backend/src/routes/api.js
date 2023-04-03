fc63c31adb3a55ed87d833acc1fbb88ece2de82a
Luke Lai <lukelai.dev@gmail.com>
2023-04-03 12:02:03 +0100

    Store and update user data after purchasing a new outfit

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -3,11 +3,13 @@ const express = require("express");
 const usersRouter = require("./users/users.router");
 const tokensRouter = require("./tokens/tokens.router");
 const gameRouter = require("./game/game.router");
+const shopRouter = require("./shop/shop.router");
 
 const api = express.Router();
 
 api.use("/users", usersRouter);
 api.use("/tokens", tokensRouter);
 api.use("/game", gameRouter);
+api.use("/shop", shopRouter);
 
 module.exports = api;
---
+++ backend/src/routes/shop/shop.controller.js
@@ -0,0 +1,18 @@
+const User = require("../../models/users.model");
+
+const ShopController = {
+  Purchase: async (req, res) => {
+    const { email, item, cost } = req.body;
+    try {
+      const user = await User.findOne({ email: email });
+      user.character.inventory.push(item);
+      user.character.attributes.coins -= cost;
+      await user.save();
+      return res.status(201).json({ message: "OK" });
+    } catch (err) {
+      return res.status(400).json({ message: "Bad request" });
+    }
+  },
+};
+
+module.exports = ShopController;
