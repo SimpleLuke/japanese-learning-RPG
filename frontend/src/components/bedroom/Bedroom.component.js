75f586f893336b470b3adce3c85e04301088c598
annamaggg <annamagnusson99@gmail.com>
2023-04-03 22:05:15 +0100

    styling complete

--- frontend/src/components/bedroom/Bedroom.component.js
+++ frontend/src/components/bedroom/Bedroom.component.js
@@ -28,8 +28,7 @@ const Bedroom = () => {
     <>
       <div className="pixelated">
     <div className="bg-bedroom2 relative bg-cover bg-center h-screen w-screen p-4 "> 
-      
-      <div className="w-60 h-24 flex flex-wrap relative left-[290px] overflow-hidden rounded-lg bg-white bg-opacity-80 px-4 py-2 shadow sm:p-3">
+      <div className="w-80 h-26 flex flex-wrap relative left-[290px] overflow-hidden border-8 border-indigo-900 bg-red-100 bg-opacity-80 px-4 py-2 shadow sm:p-2">
         <div className="w-1/2">
         <dt
           data-test="level"
@@ -56,7 +55,7 @@ const Bedroom = () => {
           Coins: {coins}
         </dt>
         </div>
-        <div className="w-1/2">
+        <div className="w-1/2 font-email absolute left-[120px]">
         <dd
           data-test="email"
           className="text-2xl font-semibold tracking-tight text-gray-900"
@@ -72,16 +71,16 @@ const Bedroom = () => {
         {character.currentOutfit && <CharacterComponent data={character.currentOutfit}/>}
       </div>
       <div className="bookshelf cursor-pointer absolute left-[377px] top-[252px]">
-        <p className="text-tag relative left-[7px]">Bookshelf</p>
+        <p className="text-tag relative left-[4px] top-[1px]">Bookshelf</p>
       </div>
       <div 
         className="study-desk cursor-pointer absolute left-[942px] top-[265px]" 
         data-test="study-desk"
         onClick={() => dispatch(setCurrentScene("START_GAME"))}>
-          <p className="text-tag absolute left-[9px] top-[3px]">Desk</p>
+          <p className="text-tag absolute left-[8px] top-[4px]">Desk</p>
       </div>
       <div className="wardrobe cursor-pointer absolute pixelated top-[485px] left-[404px]">
-        <p className="text-tag absolute left-[9px]">Wardrobe</p>
+        <p className="text-tag absolute left-[8px] top-[1px]">Wardrobe</p>
       </div>
     </div>
     </div>
