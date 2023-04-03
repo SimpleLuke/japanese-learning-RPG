b5d5233860a6f8951195f604f2e39e4a909c53b9
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-04-03 23:21:42 +0100

    Use basic Audio html tag to autoplay music on load and not re-render on every page

--- frontend/src/components/music-player/musicPlayer.component.js
+++ frontend/src/components/music-player/musicPlayer.component.js
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import useSound from "use-sound";
 import mononoke from "../../music/mononoke.mp3";
 import kiki from "../../music/kiki.mp3";
 import pokemon from "../../music/pokemon.mp3";
@@ -9,31 +8,28 @@ import spirited from "../../music/spirited.mp3";
 import wind from "../../music/wind.mp3";
 
 function MusicPlayer() {
+  const [playing, setPlaying] = useState(false);
+  const audioRef = useRef(null);
   const trackList = [mononoke, kiki, pokemon, porco, spirited, wind];
   const { toggle } = useSelector((state) => state.musicPlayer);
-  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
-  const [playbackRate, setPlaybackRate] = useState(1.0);
-  const [play, { stop }] = useSound(trackList[currentTrackIndex], {
-    playbackRate,
-    onend: () => {
-      setCurrentTrackIndex((prevIndex) => (prevIndex + 1) % trackList.length);
-    },
-  });
 
   useEffect(() => {
     if (toggle) {
-      play();
+      audioRef.current.play();
+      setPlaying(true);
     } else {
-      stop();
+      audioRef.current.pause();
+      setPlaying(false);
     }
   }, [toggle]);
 
-  useEffect(() => {
-    stop();
-    setCurrentTrackIndex(0);
-  }, [playbackRate]);
-
-  return null; // replace with your player UI
+  return (
+    <audio loop ref={audioRef}>
+      {trackList.map((track) => (
+        <source key={track} src={track} type="audio/mp3" />
+      ))}
+    </audio>
+  );
 }
 
 export default MusicPlayer;
