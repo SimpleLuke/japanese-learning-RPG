4765b901202007c58288101665278c936b70c459
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-04-03 12:25:20 +0100

    Add basic user stats to modal

--- frontend/src/components/stat-modal/statModal.component.js
+++ frontend/src/components/stat-modal/statModal.component.js
@@ -4,11 +4,16 @@ import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { CheckIcon } from "@heroicons/react/24/outline";
 import { closeStatModal } from "../../redux-store/stat-modal/statModalSlice";
+import userSlice from "../../redux-store/user/userSlice";
+import CharacterComponent from "../design-character/character.component";
 
 export default function StatModal() {
   const dispatch = useDispatch();
   const { statModalOpen } = useSelector((state) => state.statModal);
-  console.log(statModalOpen);
+  const { xp, level, wordsKnown, coins } = useSelector(
+    (state) => state.user.character.attributes
+  );
+  const { email, character } = useSelector((state) => state.user);
 
   const closeStatModalAndQuit = () => {
     dispatch(closeStatModal());
@@ -44,25 +49,21 @@ export default function StatModal() {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-                <div>
-                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                    <CheckIcon
-                      className="h-6 w-6 text-green-600"
-                      aria-hidden="true"
-                    />
-                  </div>
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-m w-full sm:p-6">
+                <div className="relative">
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
                       as="h3"
-                      className="text-base font-semibold leading-6 text-gray-900"
+                      className="text-base font-semibold leading-6 text-gray-900 mt-12"
                     >
-                      Payment successful
+                      User Stats
                     </Dialog.Title>
                     <div className="mt-2">
+                      <p className="text-sm text-gray-500">level: {level}</p>
+                      <p className="text-sm text-gray-500">XP: {xp}</p>
+                      <p className="text-sm text-gray-500">Coins: {coins}</p>
                       <p className="text-sm text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Consequatur amet labore.
+                        Words Known: {wordsKnown}
                       </p>
                     </div>
                   </div>
@@ -73,7 +74,7 @@ export default function StatModal() {
                     className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     onClick={() => closeStatModalAndQuit()}
                   >
-                    Go back to dashboard
+                    Go back to bedroom
                   </button>
                 </div>
               </Dialog.Panel>
