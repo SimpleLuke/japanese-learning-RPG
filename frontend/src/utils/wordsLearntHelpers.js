ef6ac80aecebb66e7fd55bf7223aa63e71fe6476
James Mcleish <james.mcleish@icloud.com>
2023-04-04 11:16:12 +0200

    english words added to bookshelf and duplicates removed

--- frontend/src/utils/wordsLearntHelpers.js
+++ frontend/src/utils/wordsLearntHelpers.js
@@ -9,11 +9,13 @@ const newWords = (wordsLearnt, wordsStudied) => {
   if (!wordsStudied || wordsStudied.length === 0) {
     return [];
   } else {
-    return wordsStudied.filter((pair) => {
+    const filteredWords = wordsStudied.filter((pair) => {
       return !wordsLearnt.some((learnedPair) => {
         return pair[0] === learnedPair[0] && pair[1] === learnedPair[1];
       });
     });
+    const uniqueWords = Array.from(new Set(filteredWords.map(pair => pair.join(',')))).map(str => str.split(','));
+    return uniqueWords;
   }
 };
 
