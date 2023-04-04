01386770b366a38c6bd9e8ae1ae94c2a7f6001e5
Luke Lai <lukelai.dev@gmail.com>
2023-04-04 16:03:59 +0100

    Change words in Wardrobe

--- frontend/src/components/wardrobe/Wardrobe.component.js
+++ frontend/src/components/wardrobe/Wardrobe.component.js
@@ -97,9 +97,7 @@ const Wardrobe = () => {
           <CharacterComponent data={character.currentOutfit} />
         </div>
         <div className="w-2/3 mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-xl font-bold text-gray-900">
-            Which one you want to change?
-          </h2>
+          <h2 className="text-xl font-bold text-gray-900">Pick your outfit</h2>
           <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
             {products
               .filter((item) => character.inventory.includes(item.styleName))
