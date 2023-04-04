4f9c6fc11f909175e3e3c84294ba4aeae6d17416
James Mcleish <james.mcleish@icloud.com>
2023-04-04 12:18:02 +0200

    updated tests on wordsLearntHelpers

--- frontend/src/utils/wordsLearntHelpers.cy.js
+++ frontend/src/utils/wordsLearntHelpers.cy.js
@@ -2,7 +2,7 @@ import { newWords } from "./wordsLearntHelpers";
 
 describe("newWords", () => {
   it("returns an empty array if wordsStudied is falsy", () => {
-    const wordsLearnt = ["hello", "world"];
+    const wordsLearnt = [["hello", "world"]];
     const wordsStudied = null;
 
     const result = newWords(wordsLearnt, wordsStudied);
@@ -11,7 +11,7 @@ describe("newWords", () => {
   });
 
   it("returns an empty array if wordsStudied is an empty array", () => {
-    const wordsLearnt = ["hello", "world"];
+    const wordsLearnt = [["hello", "world"]];
     const wordsStudied = [];
 
     const result = newWords(wordsLearnt, wordsStudied);
@@ -20,20 +20,29 @@ describe("newWords", () => {
   });
 
   it("filters out words that are in wordsLearnt", () => {
-    const wordsLearnt = ["hello", "world"];
-    const wordsStudied = ["hello", "world", "goodbye"];
+    const wordsLearnt = [["hello", "world"]];
+    const wordsStudied = [["hello", "world"], ["goodbye", "world"]];
 
     const result = newWords(wordsLearnt, wordsStudied);
 
-    expect(result).to.deep.equal(["goodbye"]);
+    expect(result).to.deep.equal([["goodbye", "world"]]);
   });
 
   it("returns all words if none of them are in wordsLearnt", () => {
-    const wordsLearnt = ["hello", "world"];
-    const wordsStudied = ["goodbye", "morning"];
+    const wordsLearnt = [["hello", "world"]];
+    const wordsStudied = [["goodbye", "morning"], ["hello", "morning"], ["world", "morning"]];
 
     const result = newWords(wordsLearnt, wordsStudied);
 
-    expect(result).to.deep.equal(["goodbye", "morning"]);
+    expect(result).to.deep.equal([["goodbye", "morning"], ["hello", "morning"], ["world", "morning"]]);
+  });
+
+  it("filters out duplicates from the final array", () => {
+    const wordsLearnt = [["hello", "world"], ["goodbye", "world"]];
+    const wordsStudied = [["goodbye", "world"], ["hello", "world"], ["goodbye", "world"]];
+
+    const result = newWords(wordsLearnt, wordsStudied);
+
+    expect(result).to.deep.equal([]);
   });
 });
