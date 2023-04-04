14ad24c77165fab67e896f6d8036d11cd1494d71
James Mcleish <119613083+jamesmcleish@users.noreply.github.com>
2023-04-04 12:37:46 +0200

    Update wordsLearntHelpers.js

--- frontend/src/utils/wordsLearntHelpers.js
+++ frontend/src/utils/wordsLearntHelpers.js
@@ -1,10 +1,3 @@
-// const newWords = (wordsLearnt, wordsStudied) => {
-//   if (!wordsStudied || wordsStudied.length === 0) {
-//     return [];
-//   } else {
-//     return wordsStudied.filter((word) => !wordsLearnt.includes(word));
-//   }
-// };
 const newWords = (wordsLearnt, wordsStudied) => {
   if (!wordsStudied || wordsStudied.length === 0) {
     return [];
