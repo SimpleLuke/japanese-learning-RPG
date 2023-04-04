155ec0f290b04e4603779d6a3fc009a77672d6cb
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-04-04 10:55:26 +0100

    Update sound track

--- frontend/src/components/music-player/musicPlayer.component.js
+++ frontend/src/components/music-player/musicPlayer.component.js
@@ -1,16 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import mononoke from "../../music/mononoke.mp3";
-import kiki from "../../music/kiki.mp3";
-import pokemon from "../../music/pokemon.mp3";
-import porco from "../../music/porco.mp3";
-import spirited from "../../music/spirited.mp3";
-import wind from "../../music/wind.mp3";
+import gameSoundtrack from "../../music/gameSoundtrack.wav";
 
 function MusicPlayer() {
   const [playing, setPlaying] = useState(false);
   const audioRef = useRef(null);
-  const trackList = [mononoke, kiki, pokemon, porco, spirited, wind];
+  const trackList = [gameSoundtrack];
   const { toggle } = useSelector((state) => state.musicPlayer);
 
   useEffect(() => {
