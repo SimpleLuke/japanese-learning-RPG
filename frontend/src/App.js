b60d891fb2499e040f2294a3effeabaa096fea03
annamaggg <annamagnusson99@gmail.com>
2023-04-05 12:39:32 +0100

    unnecessary files deleted and tests passing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import Login from "./components/login/Login.component";
 import Signup from "./components/signup/Signup.component";
 import Bedroom from "./components/bedroom/Bedroom.component";
 import CharacterDesign from "./components/design-character/chooseCharacter.component";
-import CustomiseCharacter from "./components/customise-character/customiseCharacter.component";
 import { useEffect } from "react";
 import { setCurrentScene } from "./redux-store/scene/sceneSlice";
 import Bookshelf from "./components/bookshelf/Bookshelf.component";
@@ -38,7 +37,6 @@ function App() {
       {currentScene === "BOOKSHELF" && <Bookshelf />}
       {currentScene === "CHARACTER" && <CharacterDesign />}
       {currentScene === "SHOP" && <Shop />}
-      {/* {currentScene === "CUSTOM_CHAR" && <CustomiseCharacter />} */}
     </div>
   );
 }
